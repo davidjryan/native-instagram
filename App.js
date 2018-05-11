c8cb9fcc7c459d12c852a69ef1f0fb0123cd25ca
David Ryan <djrdev@icloud.com>
2018-05-11 16:18:41 -0600

    app container style expo constants

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Constants } from 'expo';
 
 import Avatar from './components/Avatar';
 
@@ -7,7 +8,7 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Avatar initials={"FL"} size={35} backgroundColor={'teal'}/>
+        <Avatar initials={"FL"} size={35} backgroundColor={'teal'} />
       </View>
     )
   }
@@ -15,9 +16,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
   },
 });
