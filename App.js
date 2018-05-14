701aeba992f50f003193d326f3735107ffffba28
David Ryan <djrdev@icloud.com>
2018-05-13 18:38:39 -0600

    add author row to app

--- App.js
+++ App.js
@@ -3,12 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
 
 import Avatar from './components/Avatar';
+import AuthorRow from './components/AuthorRow';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Avatar initials={"FL"} size={35} backgroundColor={'teal'} />
+        <AuthorRow
+          fullname={'First Last'}
+          linkText={'Comments'}
+          onPressLinkText={() => {
+            console.log('Pressed Link');
+          }}
+        />
       </View>
     )
   }
